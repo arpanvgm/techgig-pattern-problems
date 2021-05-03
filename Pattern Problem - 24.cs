8d1d7b477419f8db46fd5e491c07abd266eb225a
arpanvgm <arpankumarshah@gmail.com>
2021-05-03 23:23:42 +0530

    Pattern Problem - 24

---
+++ Pattern Problem - 24.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+	//Write code here
+
+    var input = int.Parse(Console.ReadLine());
+    for(int i = 0 ; i < input ; i++){
+        var j = 1;
+        while(j < input-i){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write(' ');
+            j++;
+        }
+        while(j <= input){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write('*');
+            j++;
+        }
+        Console.WriteLine();
+    }
+    
+    }
+}
