2811330141ebd393ce9eda18762f29146c6709cc
arpanvgm <arpankumarshah@gmail.com>
2021-05-03 21:29:15 +0530

    Alphabetical Rectangle Reverse

---
+++ Pattern Problem - 8.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+	//Write code here
+    var input = int.Parse(Console.ReadLine());
+    for(int i = 1 ; i <= input ; i++){
+        for(int j = 1; j < 6; j++){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write((char)(input - i + 65));
+        }
+        Console.WriteLine();
+    }
+
+    }
+}
