8e92ff30cf0b68c4c167dc736cd181bdd8fdc5ba
arpanvgm <arpankumarshah@gmail.com>
2021-05-03 21:42:37 +0530

    Asterisk Right Angle Triangle

---
+++ Pattern Problem - 10.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+	//Write code here
+    var input = int.Parse(Console.ReadLine());
+    for(int i = 1 ; i <= input ; i++){
+        var j = 1;
+        while(j <= i){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write('*');
+            j++;
+        }
+        Console.WriteLine();
+    }
+    }
+}
