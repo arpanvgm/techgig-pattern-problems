73037ad4d7b50ddeb80d5b10e8eeb0a48f71ab5f
arpanvgm <arpankumarshah@gmail.com>
2021-05-04 08:31:47 +0530

    Pattern Problem - 37

---
+++ Pattern Problem - 37.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode {
+    static void Main(String[] args) {
+	//Write code here
+   
+    var input = int.Parse(Console.ReadLine());
+    for(int i=1 ; i<=input; i++){
+        var j = 1;
+        while(j <= input - i){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write(' ');
+            j++;
+        }
+        while(j < input + i){
+            if(j > 1){
+                Console.Write(' ');
+            }
+            Console.Write((char)(i+64));
+            j++;
+        }
+        Console.WriteLine();
+    }
+
+    }
+}
