f3c473aa2bb7054dc00f2502f26b21eb7feca2ec
arpanvgm <arpankumarshah@gmail.com>
2021-05-04 16:11:00 +0530

    1 & 2

---
+++ 001-Pattern Problem - 1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class CandidateCode
+{
+    static void Main(String[] args)
+    {
+        //Write code here
+        var input = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= input; i++)
+        {
+
+            for (int j = 1; j <= 5; j++)
+            {
+                if (j > 1)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
